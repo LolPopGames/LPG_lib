fc8cede70f49140ae58e9cf0c21b01325c2bd656
LolPopGames <lpg13.artiom@gmail.com>
2025-04-17 16:46:47 +0300

    Added assembly types

---
+++ inc/asm_types.h
@@ -0,0 +1,16 @@
+#ifndef ASM_TYPES_H_
+#define ASM_TYPES_H_
+
+// Unsigned types
+typedef unsigned char byte;
+typedef unsigned short word;
+typedef unsigned int dword;
+typedef unsigned long long qword;
+
+// Signed types
+typedef signed char sbyte;
+typedef signed short sword;
+typedef signed int sdword;
+typedef signed long long sqword;
+
+#endif
---
+++ inc/lpg.h
@@ -0,0 +1,15 @@
+#ifndef LPG_H_
+#define LPG_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <time.h>
+#include <limits.h>
+#include <ctype.h>
+
+#include "asm_types.h"
+
+#endif
