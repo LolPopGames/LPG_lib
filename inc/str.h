b5c4c3e699839b10cdfc4c82fc1985189f562a70
LolPopGames <lpg13.artiom@gmail.com>
2025-04-22 22:42:12 +0300

    Added 'str_find' func

--- inc/str.h
+++ inc/str.h
@@ -7,4 +7,7 @@
 // returns index of finded char
 sqword ch_find(const char *str, char ch);
 
+// returns index of finded string
+sqword str_find(const char *str, const char *sub_str);
+
 #endif
--- src/str.c
+++ src/str.c
@@ -2,8 +2,17 @@
 #include <string.h>
 
 // returns index of finded char
+// if it is not, returns -1
 sqword ch_find(const char *str, char ch)
 {
     char *ch_p = strchr(str, ch);
-    return ch_p - str;
+    return (ch_p) ? ch_p-str : -1;
+}
+
+// returns index of finded string
+// if it is not, returns -1
+sqword str_find(const char *str, const char *sub_str)
+{
+    char *str_p = strstr(str, sub_str);
+    return (str_p) ? str_p-str : -1;
 }
